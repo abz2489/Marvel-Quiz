59175874c250ce667955a3e7fbb958253046fea9
Abigail Davies <abz2489@gmail.com>
2023-06-10 20:38:22 +0000

    Add quiz questions choices and correct answer

---
+++ assets/scripts/questions.js
@@ -0,0 +1,61 @@
+[
+    {
+        "question": "How many infinity stones are there in the Marvel Universe?",
+        "choices": ["10", "6","3","8"],
+        "correct": "6"
+    },
+ 
+    {
+        "question": "What is the name of the cube that Loki utilizes to power up his weapons?",
+        "choices": ["Tetracube", "Trimurti","Trimeric","Tesseract"],
+        "correct": "Tesseract"
+    },
+
+    {
+        "question": "Which country is the birthplace of Black Widow",
+        "choices": ["Canada" ,"America" ,"South Africa" ,"Russia"],
+        "correct": "Russia"
+    },
+
+    {
+        "question": "What was the first movie that started the Marvel Cinematic Universe?",
+        "choices": ["Iron Man", "The Incredible Hulk", "Captain America: The First Avenger", "Thor"],
+        "correct": "Iron Man"
+    },
+
+    {
+        "question": "Who was able to pick up Thor’s sword in Avengers: Endgame?",
+        "choices": ["Hulk", "Captain America", "Captain Marvel", "Spiderman"],
+        "correct": "Captain America"
+    },
+
+    {
+        "question": "What is Thor’s ax’s name that was created for him in Avengers: Infinity War?",
+        "choices": ["Heartbreaker", "Windbreaker", "Stormbreaker", "Mjölnir"],
+        "correct": "Stormbreaker"
+    },
+
+    {
+        "question": "Who is the first person that turns to dust in Avengers: Infinity War?",
+        "choices": ["Falcon", "Bucky", "Groot", "Black Panther"],
+        "correct": "Bucky"
+    },
+
+    {
+        "question": "What’s the name of Tony Stark’s company?",
+        "choices": ["Stark Enterprise", "Stark Worldwide", "Stark Industries", "Stark Superheroes"],
+        "correct": "Stark Industries"
+    },
+
+    {
+        "question": "What is T’Challa’s sister’s name?",
+        "choices": ["Shari", "Shaba", "Shami", "Shuri"],
+        "correct": "Shuri"
+    },
+
+    {
+        "question": "Who is the main antagonist in the first Avengers movie?",
+        "choices": ["Thanos", "Ego", "Abomination", "Loki"],
+        "correct": "Loki"
+    }
+]
