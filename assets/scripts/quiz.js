a72df9627931d5b68280462262f11088c010ab2c
Abigail Davies <abz2489@gmail.com>
2023-06-11 07:31:56 +0000

    Get html elements and save as const variables

---
+++ assets/scripts/quiz.js
@@ -0,0 +1,10 @@
+// Get HTML elements and save as const variables
+const howToPlayBtn = document.querySelector(".btn-how-to-play");
+const startQuizBtn = document.querySelector(".btn-start-quiz");
+const quizContainer = document.querySelector(".quiz-container");
+const scoreboard = document.querySelector("#scoreboard");
+const scoreCounter = document.querySelector(".score-counter");
+const quitGame = document.querySelector(".quit-game");
+const questionContent = document.querySelector("#question");
+const nextButton = document.querySelector("#next-btn");
+const choicesContainer = document.querySelector(".choices-container");
