dab42f0ba5ec309497924cb8551b69a4eea940e6
Abigail Davies <abz2489@gmail.com>
2023-06-19 08:52:32 +0000

    Update modal to how to play modal

--- assets/scripts/modal.js
+++ assets/scripts/modal.js
@@ -1,13 +1,13 @@
 // How to Play modal learnt using https://www.freecodecamp.org/news/how-to-build-a-modal-with-javascript/
-const modal = document.querySelector(".modal");
+const howToPlayModal = document.querySelector(".how-to-play-modal");
 const startQuiz = document.querySelector(".btn-start-quiz");
 
 // Function to open how to Play modal
 function openModal() {
-    modal.classList.remove("hidden");
+    howToPlayModal.classList.remove("hidden");
 };
 
 // Function to close How to Play modal
 function closeModal() {
-    modal.classList.add("hidden");
+    howToPlayModal.classList.add("hidden");
 };
